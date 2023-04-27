5fc581175fc9248059f2253d5462c17a60210b6c
Will Cote <90800455+willcote@users.noreply.github.com>
2023-04-27 04:32:57 -0400

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const convertToCelsius = function() {
+// Probably better to use Math.round(someValue * 10) / 10
+// to get rounding for a single decimal place.
+
+const convertToCelsius = function (temperature) {
+  return parseFloat(((temperature - 32) * (5 / 9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  return parseFloat((temperature * (9 / 5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
