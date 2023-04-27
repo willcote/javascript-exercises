5ae2654789d5d95785c613f5567feacbf38bb0be
Will Cote <90800455+willcote@users.noreply.github.com>
2023-04-27 05:09:33 -0400

    Complete 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+  let titles = [];
+  for (const book of books) {
+    titles.push(book["title"]);
+  }
 
+  return titles;
 };
 
 // Do not edit below this line
