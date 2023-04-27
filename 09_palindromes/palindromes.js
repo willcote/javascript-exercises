48d19826de19eaba917eee49e4d1b464c99f7df3
Will Cote <90800455+willcote@users.noreply.github.com>
2023-04-27 04:53:15 -0400

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  /* 
+  clean input
+  create a new string with reversed input
+  return comparison
+  */
 
+  str = str.replace(/[^a-zA-Z]+/g, "").toLowerCase();
+  let reverse = str.split("").reverse().join("");
+  return str === reverse;
 };
 
 // Do not edit below this line
