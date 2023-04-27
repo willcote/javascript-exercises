d49a61d9e69bb42398ebc4e9f2529d550eb865f6
Will Cote <90800455+willcote@users.noreply.github.com>
2023-04-27 05:01:25 -0400

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  num = parseInt(num);
 
+  if (num === 1 || num === 2) return 1;
+
+  let prev = 1;
+  let total = 2;
+  for (let i = 3; i < num; i++) {
+    let temp = total;
+    total += prev;
+    prev = temp;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
