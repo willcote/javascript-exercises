7de47287a0c9aba2c35dd9373f1602d2e82b7bd3
Will Cote <90800455+willcote@users.noreply.github.com>
2023-04-27 04:11:38 -0400

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  // write down all the numbers between 1 and 2 and add them together
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    typeof num1 !== "number" ||
+    typeof num2 !== "number"
+  )
+    return "ERROR";
 
+  let lowerNumber = num1 < num2 ? num1 : num2;
+  let higherNumber = num1 >= num2 ? num1 : num2;
+
+  let total = 0;
+  for (let i = lowerNumber; i <= higherNumber; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
