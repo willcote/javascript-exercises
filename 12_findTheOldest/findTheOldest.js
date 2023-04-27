4a38c997946e0c8b71391ebaeae7c4e19923d679
Will Cote <90800455+willcote@users.noreply.github.com>
2023-04-27 05:23:50 -0400

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldestPerson;
+  let oldestAge = 0;
+  for (const person of people) {
+    let age;
+    let date = new Date();
+    if (!person.hasOwnProperty("yearOfDeath"))
+      age = date.getFullYear() - person.yearOfBirth;
+    else age = person.yearOfDeath - person.yearOfBirth;
 
+    if (age > oldestAge) {
+      oldestPerson = person;
+      oldestAge = age;
+    }
+  }
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
