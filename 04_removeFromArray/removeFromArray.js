f9e9c92d49889d5a1f80e3dd235e1bc5cb8909d8
Will Cote <90800455+willcote@users.noreply.github.com>
2023-04-27 03:56:55 -0400

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...remove) {
+  for (rem of remove) {
+    for (let i = 0; i < arr.length; i++) {
+      if (rem === arr[i]) {
+        arr.splice(i, 1);
+      }
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
