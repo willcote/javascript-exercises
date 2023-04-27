520280315e985014e78cd6b47935ce14730d9d47
Will Cote <90800455+willcote@users.noreply.github.com>
2023-04-27 04:42:58 -0400

    Complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arrayOfNumbers) {
+  let total = 0;
+  for (const number of arrayOfNumbers) {
+    total += number;
+  }
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function (arrayOfNumbers) {
+  let total = 1;
+  for (const number of arrayOfNumbers) {
+    total *= number;
+  }
 
+  return total;
 };
 
-const power = function() {
-	
+const power = function (num, power) {
+  return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let total = 1;
+  if (num === 0) return 1;
+  else {
+    for (let i = num; i > 0; i--) {
+      total *= i;
+    }
+  }
+
+  return total;
 };
 
 // Do not edit below this line
@@ -29,5 +47,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
