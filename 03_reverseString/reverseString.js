d34a3576af3abb622aff0e99102dd0cccece20a5
Will Cote <90800455+willcote@users.noreply.github.com>
2023-04-27 01:51:18 -0400

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  let strArray = str.split("");
+  let reversedString = "";
 
+  // this works
+  for (let i = strArray.length - 1; i >= 0; i--) {
+    reversedString += strArray[i];
+  }
+
+  return reversedString;
 };
 
 // Do not edit below this line
