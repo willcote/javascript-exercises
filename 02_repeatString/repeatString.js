4c9278d5664fdf94d84ddd0eaf54bd209aee3c3b
Will Cote <90800455+willcote@users.noreply.github.com>
2023-04-27 01:29:27 -0400

    Complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+  if (num < 0) return "ERROR";
 
+  let fullString = "";
+  for (let i = 0; i < num; i++) {
+    fullString += str;
+  }
+  return fullString;
 };
 
 // Do not edit below this line
